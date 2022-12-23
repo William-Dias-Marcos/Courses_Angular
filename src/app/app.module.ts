b5df799fd18fbd1a5759ae8291c283626ac44866
William Dias Marcos <william.diasmarcos@gmail.com>
2022-12-22 23:23:24 -0300

    footer creation/ component editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesModule } from './components/courses/courses.module';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, CoursesModule],
   providers: [],
   bootstrap: [AppComponent],
